c6e3563d19081dea4b8e9ac30f215e9fa9fcca3a
ThipnaphaP <90165823+ThipnaphaP@users.noreply.github.com>
2021-09-06 16:36:39 +0700

    Add files via upload

---
+++ demoapp/src/test/java/com/example/demoapp/employees/EmployeeControllerWebMvcTest.java
@@ -0,0 +1,36 @@
+package com.example.demoapp.employees;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+
+@WebMvcTest(EmployeeController.class)
+public class EmployeeControllerWebMvcTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getEmployeeById() throws Exception {
+        int id = 100;
+        MvcResult result = mvc.perform(get("/employees/"+ id))
+                .andExpect(status().isOk())
+                .andReturn();
+        // Response body
+        byte[] json = result.getResponse().getContentAsByteArray();
+        ObjectMapper mapper = new ObjectMapper();
+        EmployeeResponse response = mapper.readValue(json, EmployeeResponse.class);
+        // Assert
+        assertEquals(id, response.getId());
+        assertEquals("somkiat", response.getName());
+        
+    }
+}
