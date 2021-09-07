b8bf6d21d13991843e21fed5041a746864d54bb1
ThipnaphaP <90165823+ThipnaphaP@users.noreply.github.com>
2021-09-07 10:47:11 +0700

    Update EmployeeControllerTest.java

--- demoapp/src/test/java/com/example/demoapp/employees/EmployeeControllerTest.java
+++ demoapp/src/test/java/com/example/demoapp/employees/EmployeeControllerTest.java
@@ -1,16 +1,36 @@
+
 package com.example.demoapp.employees;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class EmployeeControllerTest {
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class EmployeeControllerTest {
+
     @Autowired
     private TestRestTemplate restTemplate;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    public void getEmployeeById() {
+        // Arrange
+        int id = 1;
+        Employee employee100 = new Employee();
+        employee100.setName("thipnapha");
+        employeeRepository.save(employee100);
+        // Act
+        EmployeeResponse result
+                = restTemplate.getForObject("/employees/" + id, EmployeeResponse.class);
+        // Assert
+        assertEquals(id, result.getId());
+        assertEquals("thipnapha", result.getName());
+    }
 
     @Test
     public void listEmployee() {
@@ -20,16 +40,6 @@ class EmployeeControllerTest {
         // Assert
         assertEquals(2, results.length);
         assertEquals(1, results[0].getId());
-        assertEquals("Name1", results[0].getName());
+        assertEquals("thipnapha", results[0].getName());
     }
-
-    @Test
-    public void listEmployeeById() {
-        // Act
-        EmployeeResponse result
-                = restTemplate.getForObject("/employees/1", EmployeeResponse.class);
-        // Assert
-        assertEquals(1, result.getId());
-        assertEquals("Name1", result.getName());
-    }
-}
+ }
