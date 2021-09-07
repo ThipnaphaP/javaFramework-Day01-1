1fb6f242e055f5921fd6f8bf54c821b46d1f7a29
ThipnaphaP <90165823+ThipnaphaP@users.noreply.github.com>
2021-09-07 14:43:29 +0700

    Update EmployeeService.java

--- demoapp/src/main/java/com/example/demoapp/employees/EmployeeService.java
+++ demoapp/src/main/java/com/example/demoapp/employees/EmployeeService.java
@@ -7,10 +7,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
-	
-	@Autowired
 	private EmployeeRepository employeeRepository;
-
+	@Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 	public EmployeeResponse getById(int id) {
 
         Optional<Employee> result = employeeRepository.findById(id);
@@ -26,3 +27,4 @@ public class EmployeeService {
 		
 	}
 }
+
